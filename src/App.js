050535b016859348f0747c1a115347b85f81d4b3
HrithikMittal <adhikanshmittal@gmail.com>
2021-01-16 20:28:50 +0530

    create different store

--- src/App.js
+++ src/App.js
@@ -14,29 +14,9 @@ import {
   Table,
 } from "semantic-ui-react";
 
-const ContactContext = React.createContext();
+import initialState from "./store";
 
-const initialState = {
-  contacts: [
-    {
-      id: "007",
-      name: "Adhikansh Mittal",
-      email: "amittal@dashclicks.com",
-    },
-    {
-      id: "008",
-      name: "Devandra Prajapat",
-      email: "dprajapat@dashclicks.com",
-    },
-    {
-      id: "009",
-      name: "Vikas Agarwal",
-      email: "vagarwal@dashclicks.com",
-    },
-  ],
-  loading: false,
-  error: null,
-};
+const ContactContext = React.createContext();
 
 const reducer = (state, action) => {
   switch (action.type) {
---
+++ src/store.js
@@ -0,0 +1,23 @@
+const initialState = {
+  contacts: [
+    {
+      id: "007",
+      name: "Adhikansh Mittal",
+      email: "amittal@dashclicks.com",
+    },
+    {
+      id: "008",
+      name: "Devandra Prajapat",
+      email: "dprajapat@dashclicks.com",
+    },
+    {
+      id: "009",
+      name: "Vikas Agarwal",
+      email: "vagarwal@dashclicks.com",
+    },
+  ],
+  loading: false,
+  error: null,
+};
+
+export default initialState;
